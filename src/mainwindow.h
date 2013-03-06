c3dec09c995d6bf2235c1e487bfbe903ad855330
frans <frans@scitics.de>
2013-03-06 08:21:01 +0100

    progress

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,24 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-/*
-(L) Requirements for Flow
-    [ ] having an Inbox
-    [ ] having contexts
-    [ ] having sub contexts which can be subcontext elsewhere too
-    [ ] contexts can have files opened
-    [ ] files can get synchronized to wikis
-    [ ] switching tasks etc gets logged
+#include "ui_brainstack.h"
 
-    [ ] data being locally encrypted
-    [ ] jounaled file based -> conflict safe
-
-    [ ] Inbox items can be added without authorization (eg by others)
-    [ ] service based
-*/
-
-#include "ui_flow2.h"
-#include "FlowModel.h"
+#include "codeeditor.h"
 
 #include <map>
 #include <QtGui/QMainWindow>
@@ -36,6 +21,17 @@
 #include <stdio.h>
 #include <assert.h>
 
+// [X] close items
+// [ ] track tasks - recognize begin, end
+// [ ] add/close plausibility checks
+// [ ] backup saving
+// [ ] link items
+// [ ] find on typing
+// [ ] save in a readable way
+// [ ] connect to ysbox
+// [ ] be SparkleShare compatible
+// [ ] have a standard set of open files (per task?)
+
 inline void tracemessage( const char * a_format, ... )
 {
     char l_buffer[1024];
@@ -45,6 +41,136 @@ inline void tracemessage( const char * a_format, ... )
     va_end( l_args );
     printf( "%s\n", l_buffer );
     fflush( stdout );
+}
+
+class Task
+{
+public:
+
+    Task()
+        : m_text            ( "" )
+        , m_active          ( true )
+        , m_summarizedTime  ( 0 )
+    {
+        /// don't add vital code here - this is just for
+        /// some containers needing a standard constructor
+        m_durationMeter.start();
+    }
+
+    Task( const QString &text )
+        : m_text            ( text )
+        , m_active          ( true )
+        , m_summarizedTime  ( 0 )
+    {
+        m_durationMeter.start();
+        m_durationMeter.restart();
+        tracemessage( "%d", m_durationMeter.elapsed() );
+    }
+
+    void activate()
+    {
+        m_activeTimes.append(
+                    QPair< QDateTime, QDateTime >(
+                        QDateTime::currentDateTime(),QDateTime()) );
+        m_durationMeter.restart();
+    }
+
+    void deactivate()
+    {
+        assert( !m_activeTimes.empty() );
+        m_activeTimes.back().second = QDateTime::currentDateTime();
+        m_summarizedTime += m_durationMeter.elapsed();
+    }
+
+    void close()
+    {
+        deactivate();
+    }
+
+    int getDuration() const
+    {
+        // tracemessage( "%d", m_durationMeter.elapsed() );
+        return m_durationMeter.elapsed();
+    }
+
+    bool m_active;
+    QString m_text;
+    int m_summarizedTime;
+    QTime m_durationMeter;
+    QList< QPair< QDateTime,QDateTime > > m_activeTimes;
+};
+
+class Model
+{
+public:
+    void activate( const QString &taskName )
+    {
+        QMap< QString, Task >::iterator i( m_texts.find( taskName ) );
+
+        /// does a task with the given name exist?
+        if( m_texts.find( taskName ) == m_texts.end() )
+        {
+            i = m_texts.insert( taskName, Task("") );
+        }
+
+        i->activate();
+
+        m_currentTask = taskName;
+    }
+
+    void deactivate();
+
+    bool hasCurrentTask() const
+    {
+        return m_currentTask != "";
+    }
+
+    const QString & getCurrentTaskNotes() const
+    {
+        assert( hasCurrentTask() );
+
+        assert( m_texts.find( m_currentTask ) != m_texts.end() );
+
+        return m_texts.find( m_currentTask )->m_text;
+    }
+
+    int getCurrentTaskDuration()
+    {
+        if( !hasCurrentTask() ) return 0;
+
+        assert( m_texts.find( m_currentTask ) != m_texts.end() );
+
+        return m_texts.find( m_currentTask )->getDuration();
+    }
+
+    void setCurrentNote( const QString &text )
+    {
+        ///.current task must be set
+        assert( m_currentTask != "" || text == "" );
+
+        if( m_currentTask == "" ) return;
+
+        assert( m_texts.find( m_currentTask ) != m_texts.end() );
+
+        m_texts[ m_currentTask ].m_text = text;
+    }
+
+    QMap< QString, Task >   m_texts;
+    QString                 m_currentTask;
+};
+
+/// serialization
+inline QDataStream & operator<<( QDataStream &out, const Task &task )
+{
+    out << task.m_active << task.m_text;
+    return out;
+}
+
+/// deserialization
+inline QDataStream & operator>>( QDataStream &in, Task &task )
+{
+    in >> task.m_active >> task.m_text;
+    return in;
 }
 
 class MainWindow
@@ -67,7 +193,7 @@ public:
 
         m_ui->setupUi( this );
 
-        //m_ui->teNotes->setEnabled( false );
+        m_ui->teNotes->setEnabled( false );
 
         importFromFs();
 
@@ -88,9 +214,13 @@ private:
 
         QMutexLocker monitor( &m_mutex );
 
+        /// before we change the task - write back all changes
+        m_model.setCurrentNote( m_ui->teNotes->toPlainText() );
 
-//        m_ui->teNotes->setPlainText( m_model.getCurrentTaskNotes() );
-//        m_ui->teNotes->setEnabled( m_model.hasCurrentTask() );
+        m_model.activate( taskName );
+
+        m_ui->teNotes->setPlainText( m_model.getCurrentTaskNotes() );
+        m_ui->teNotes->setEnabled( m_model.hasCurrentTask() );
     }
 
     void exportToFs()
@@ -101,10 +231,9 @@ private:
         QFile l_file( "file.dat" );
         l_file.open( QIODevice::WriteOnly );
         QDataStream l_dsOut( &l_file );
-        //l_dsOut << m_model.m_texts;
+        l_dsOut << m_model.m_texts;
 
         tracemessage("export %d values:", m_model.m_texts.size() );
-        /*
         QMapIterator< QString, Task > i( m_model.m_texts );
         while( i.hasNext() )
         {
@@ -115,7 +244,6 @@ private:
                          i.value().m_text.toAscii().data() );
             m_ui->lwTask->addItem( i.key() );
         }
-        */
     }
 
     void importFromFs()
@@ -126,10 +254,9 @@ private:
         QFile l_file("file.dat");
         l_file.open( QIODevice::ReadOnly );
         QDataStream l_dsIn( &l_file );
-        //l_dsIn >> m_model.m_texts;
-        /*
-        tracemessage("imported %d items:", m_model.m_texts.size() );
+        l_dsIn >> m_model.m_texts;
 
+        tracemessage("imported %d items:", m_model.m_texts.size() );
         QMapIterator< QString, Task > i( m_model.m_texts );
         while( i.hasNext() )
         {
@@ -139,13 +266,12 @@ private:
                          i.value().m_active?"active":"closed",
                          i.value().m_text.toAscii().data() );
         }
-        */
         updateGui();
     }
 
     void updateGui()
     {   tracemessage( __FUNCTION__ );
-/*
+
         m_ui->lwTask->clear();
 
         QMapIterator< QString, Task > i( m_model.m_texts );
@@ -159,20 +285,18 @@ private:
                 m_ui->lwTask->addItem( l_item );
             }
         }
-        */
     }
 
     void closeEvent( QCloseEvent *event )
     {   tracemessage( __FUNCTION__ );
 
-        //m_model.setCurrentNote( m_ui->teNotes->toPlainText() );
+        m_model.setCurrentNote( m_ui->teNotes->toPlainText() );
 
         exportToFs();
     }
 
     void dump()
     {
-        /*
         tracemessage("currently model contains %d items:", m_model.m_texts.size() );
         QMapIterator< QString, Task > i( m_model.m_texts );
         while( i.hasNext() )
@@ -183,14 +307,13 @@ private:
                          i.value().m_active?"active":"closed",
                          i.value().m_text.toAscii().data() );
         }
-        */
     }
 
 private slots:
 
     void update()
     {   //tracemessage( __FUNCTION__ );
-        //m_ui->lblTask->setText( QString().sprintf("%d",m_model.getCurrentTaskDuration()) );
+        m_ui->lblTask->setText( QString().sprintf("%d",m_model.getCurrentTaskDuration()) );
         //tracemessage( "%d",m_model.getCurrentTaskDuration() );
     }
 
@@ -209,15 +332,14 @@ private slots:
     void on_pbAdd_clicked()
     {   tracemessage( __FUNCTION__ );
 
-        //m_ui->lwTask->addItem( m_ui->leCommand->text() );
-        //m_ui->leCommand->clear();
+        m_ui->lwTask->addItem( m_ui->leCommand->text() );
+        m_ui->leCommand->clear();
     }
 
     void on_pbClose_clicked()
     {   tracemessage( __FUNCTION__ );
 
         //QMutexLocker monitor( &m_mutex );
-        /*
         tracemessage( "close task '%s'", m_model.m_currentTask.toAscii().data() );
 
         QMap< QString, Task >::iterator i( m_model.m_texts.find( m_model.m_currentTask ) );
@@ -231,10 +353,10 @@ private slots:
         assert( m_ui->lwTask->currentItem()->text() == m_model.m_currentTask );
         QListWidgetItem *l_item = m_ui->lwTask->currentItem();
         assert( l_item != NULL );
-        */
+
         // will handle deletion from list and synchronously make the
         // list widget update - so this is a potential deadlock source!
-        //delete l_item;
+        delete l_item;
     }
 
     void on_leCommand_returnPressed ()
