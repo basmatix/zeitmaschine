d6bbb9d5cbb4627c9c5a535305bedf1e92b70ba8
frans <frans@scitics.de>
2014-01-17 09:22:18 +0100

    wrapped bimap into ModelData

--- src/mm_lib/include/mm/zmModel.h
+++ src/mm_lib/include/mm/zmModel.h
@@ -26,29 +26,48 @@ namespace zm
     class ChangeSet;
     class zmOptions;
 
+    /// rebuilds a string <=> MindMatter bimap interface and provides
+    /// Map related functionality like serialization
     class MindMatterModel
     {
     public:
 
-
         class ModelData
-                : public boost::bimaps::bimap< std::string, MindMatter * >
         {
             typedef boost::bimaps::bimap< std::string, MindMatter * > uid_mm_bimap_t;
             uid_mm_bimap_t m_data;
+
         public:
-            /*
+
+            ModelData()
+                : m_data    ()
+                , right     (m_data.right)
+                , left      (m_data.left)
+            { }
+
             typedef uid_mm_bimap_t::left_const_iterator left_const_iterator;
             typedef uid_mm_bimap_t::left_iterator left_iterator;
+            typedef uid_mm_bimap_t::right_const_iterator right_const_iterator;
+            typedef uid_mm_bimap_t::right_iterator right_iterator;
+            typedef uid_mm_bimap_t::const_iterator const_iterator;
+            typedef uid_mm_bimap_t::iterator iterator;
+
             typedef uid_mm_bimap_t::value_type value_type;
-            virtual ~ModelData(){};
-            size_t size() const
-            {
-                return m_data.size();
-            }
-            left_iterator begin();
-            left_iterator end();
-            */
+
+            virtual ~ModelData(){}
+
+            size_t size() const {return m_data.size();}
+            bool empty() const {return m_data.empty();}
+            void clear() {m_data.clear();}
+            void insert(value_type a_pair) {m_data.insert(a_pair);}
+            uid_mm_bimap_t::iterator begin() {return m_data.begin();}
+            uid_mm_bimap_t::iterator end() {return m_data.end();}
+            uid_mm_bimap_t::const_iterator begin() const {return m_data.begin();}
+            uid_mm_bimap_t::const_iterator end() const{return m_data.end();}
+
+            uid_mm_bimap_t::left_map  &left;
+            uid_mm_bimap_t::right_map &right;
+
         };
 
     private:
