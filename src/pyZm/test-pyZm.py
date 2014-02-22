c7d236feb8a19f61167e99e6991f1f6ae053ec57
frans <frans@scitics.de>
2014-02-22 13:08:14 +0100

    example python file

--- src/pyZm/test-pyZm.py
+++ src/pyZm/test-pyZm.py
@@ -1,6 +1,45 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# file: test-pyZm.py
+#
+# Copyright (C) 2014 Frans Fuerst
+
+from optparse import OptionParser
+import sys
+import logging
 
 import pyZm
 
-if __name__ == '__main__':
-    print "i survived!"
+def list_all():
+
+    model = pyZm.MindMatterModel()
+    #model.setLocalFolder( "./test-localfolder" )
+    #model.setUsedUsername( "test-user" )
+    #model.setUsedHostname( "test-machine" )
+    model.disableHashChecking()
+    #model.setTraceLevel(4)
+    model.initialize()
+    print model.getItemCount()
+
+def main():
+    parser = OptionParser()
+    parser.add_option("-v", "--verbose", dest="verbose", default=False,
+                      action="store_true", help="set verbosity on", metavar="verbosity")
+    (options, args) = parser.parse_args()
+    list_all()
+    
+    
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s [test] %(levelname)s %(message)s",
+        datefmt="%y%m%d-%H%M%S",
+        level=logging.DEBUG)
+    logging.addLevelName( logging.CRITICAL, "(CRITICAL)" )
+    logging.addLevelName( logging.ERROR,    "(EE)" )
+    logging.addLevelName( logging.WARNING,  "(WW)" )
+    logging.addLevelName( logging.INFO,     "(II)" )
+    logging.addLevelName( logging.DEBUG,    "(DD)" )
+    logging.addLevelName( logging.NOTSET,   "(NA)" )
+
+    main()
