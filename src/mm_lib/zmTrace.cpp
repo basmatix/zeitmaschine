538790b15201212afb536db3a3c227df5e72fa96
frans <frans@scitics.de>
2014-03-16 16:36:11 +0100

    make Android tracer use logcat

--- src/mm_lib/zmTrace.cpp
+++ src/mm_lib/zmTrace.cpp
@@ -5,9 +5,6 @@
 /// Copyright (C) 2013 Frans Fuerst
 ///
 
-#include <stdarg.h>
-#include <stdio.h>
-
 //0: off
 //1: critical
 //2: error
@@ -22,6 +19,45 @@ void set_trace_level( int level )
 {
     g_trace_level = level;
 }
+
+#if defined(ANDROID)
+
+#include <android/log.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#define  LOG_TAG    "zm"
+#define  ALOG(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG,__VA_ARGS__)
+
+void trace_i( const char * a_format, ... )
+{
+    if(g_trace_level < 4) return;
+
+    char l_buffer[1024];
+    va_list l_args;
+    va_start (l_args, a_format);
+    vsnprintf ( l_buffer, 1023, a_format, l_args );
+    va_end( l_args );
+    ALOG("(II) %s", l_buffer );
+}
+
+void trace_e( const char * a_format, ... )
+{
+    if(g_trace_level < 2) return;
+
+    char l_buffer[1024];
+    va_list l_args;
+    va_start (l_args, a_format);
+    vsnprintf ( l_buffer, 1023, a_format, l_args );
+    va_end( l_args );
+    ALOG("(EE) %s", l_buffer );
+    ALOG("(EE) at %s:%d", __FILE__, __LINE__);
+}
+
+#else
+
+#include <stdarg.h>
+#include <stdio.h>
 
 void trace_i( const char * a_format, ... )
 {
@@ -48,3 +84,6 @@ void trace_e( const char * a_format, ... )
     printf( "%s\n", l_buffer );
     fflush( stdout );
 }
+#endif
+
+
